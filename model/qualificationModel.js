1fc41a1e280b3dc9e7dfe1b8aea3b46e3dc11042
Krisztian <krisztian.valyi1@gmail.com>
2023-03-30 15:34:22 +0100

    rewrote qualifcaition model

--- model/qualificationModel.js
+++ model/qualificationModel.js
@@ -3,56 +3,56 @@ import validator from "validator";
 const Schema = mongoose.Schema;
 
 const qualificationModel = new Schema({
-    imageURL: {
-        type: String,
-        required: true
-    },
-    grade: {
-        type: String,
-        required: true
-    },
-    subjectName: {
-        type: Number,
-        required: true
-    },
-    institutionName: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    from: {
-        type: Date,
-        required: true
-    },
-    to: {
-        type: Date,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    weight: {
-        type: String,
-        required: true
-    },
-    qualificationLevel: {
-        type: String,
-        required: true
-    },
-    priority: {
-        type: Number,
-        required: true
-    },
-    user_id: {
-        type: String,
-        required: true
-    }
-})
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+  subjectName: {
+    type: String,
+    required: true,
+  },
+  institutionName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  from: {
+    type: Date,
+    required: true,
+  },
+  to: {
+    type: Date,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: String,
+    required: true,
+  },
+  qualificationLevel: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: Number,
+    required: true,
+  },
+  user_id: {
+    type: String,
+    required: true,
+  },
+});
 
-const Qualification = mongoose.model('qualification', qualificationModel);
+const Qualification = mongoose.model("qualification", qualificationModel);
 
-export default Qualification
+export default Qualification;
